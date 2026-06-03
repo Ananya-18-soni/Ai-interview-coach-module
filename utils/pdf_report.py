5a5aed834db3dda782023dfe97580ba4c214c5b2
Ananya-18-soni <soniananya1802@gmail.com>
2026-06-03 11:17:36 +0530

    Update pdf_report.py

--- utils/pdf_report.py
+++ utils/pdf_report.py
@@ -1,22 +1,30 @@
 from fpdf import FPDF
 
 
-def generate_report(role, feedback, filename):
+def generate_report(title, feedback, filename):
 
     pdf = FPDF()
 
     pdf.add_page()
 
     pdf.set_font("Arial", "B", 16)
-    pdf.cell(200, 10, "AI Interview Report", ln=True)
+
+    pdf.cell(
+        200,
+        10,
+        txt=title,
+        ln=True,
+        align="C"
+    )
 
     pdf.ln(10)
 
-    pdf.set_font("Arial", "", 12)
+    pdf.set_font("Arial", size=12)
+
     pdf.multi_cell(
         0,
         10,
-        f"Role: {role}\n\nFeedback:\n{feedback}"
+        feedback
     )
 
     pdf.output(filename)
